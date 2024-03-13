2a362b9a506962b0f39b4e5d70e3b44f6b2e54cb
Yuria-Shikibe <3415339403@qq.com>
2024-03-13 23:18:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-#include <chrono>
-#include <iostream>
-#include <thread>
-
 //Local Compile doesn't have any failure
 //But fails in Github Workflow
 //Use Both MSVC 17.9.5+33de0b227
@@ -9,6 +5,10 @@
 // Haven't Test Include Header File yet
 // Only Module triggers compile failure, currently
 import ChronoTest;
+
+#include <chrono>
+#include <iostream>
+#include <thread>
 
 int main(){
 	//must invoke this, should be the problem of -/+ operation between std::chrono::duration;
