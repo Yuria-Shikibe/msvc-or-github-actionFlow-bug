b5fafc72add8e70259d3433f67c102c873d1e9df
Yuria-Shikibe <3415339403@qq.com>
2024-03-13 23:36:10 +0800

    Update chrono_test.h

--- src/chrono_test.h
+++ src/chrono_test.h
@@ -5,7 +5,7 @@
 #ifndef CHRONO_TEST_H
 #define CHRONO_TEST_H
 
-#include <chrono>;
+#include <chrono>
 
 namespace Test{
 	auto getSpacing(const std::chrono::milliseconds p1, const std::chrono::milliseconds p2){
