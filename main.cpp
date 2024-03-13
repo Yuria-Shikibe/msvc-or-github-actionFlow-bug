598a478d91f5dc47c8ea2d8ca80fac3b590f5011
Yuria-Shikibe <3415339403@qq.com>
2024-03-13 23:01:05 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,13 +15,15 @@ struct Test02 {
 };
 
 int main(){
-	Test01 test01{};
-	std::cout << test01.getSpacing(std::chrono::milliseconds(10), std::chrono::microseconds(20)) << std::endl;
+	// Test01 test01{};
+	// std::cout << test01.getSpacing(std::chrono::milliseconds(10), std::chrono::microseconds(20)) << std::endl;
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	std::this_thread::sleep_for(std::chrono::microseconds(100));
+	std::this_thread::sleep_for(std::chrono::nanoseconds(100));
 
-	Test02 test02{};
-	std::cout << test02.getSpacing(std::chrono::milliseconds(10), std::chrono::milliseconds(20)) << std::endl;
+	// Test02 test02{};
+	// std::cout << test02.getSpacing(std::chrono::milliseconds(10), std::chrono::milliseconds(20)) << std::endl;
 
 	return 0;
 }
