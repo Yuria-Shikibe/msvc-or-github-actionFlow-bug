b433e38aad3e01aaba05bf1f5f4d566f1536a5d5
Yuria-Shikibe <3415339403@qq.com>
2024-03-13 22:54:55 +0800

    -

--- main.cpp
+++ main.cpp
@@ -12,5 +12,8 @@ int main(){
 	std::this_thread::sleep_for(std::chrono::microseconds(10));
 
 	std::cout << test01.getSpacing(std::chrono::microseconds(10), std::chrono::nanoseconds(20)) << std::endl;
+
+	Test::Test02 test02{};
+	std::cout << test02.getSpacing(std::chrono::milliseconds(10), std::chrono::milliseconds(20)) << std::endl;
 	return 0;
 }
--- src/ChronoTest02.cppm
+++ src/ChronoTest02.cppm
@@ -4,7 +4,9 @@ import <chrono>;
 
 export namespace Test{
 	struct Test02 {
+		int test;
 		auto getSpacing(std::chrono::milliseconds p1, std::chrono::milliseconds p2){
+			test--;
 			return p1 - p2;
 		}
 	};
