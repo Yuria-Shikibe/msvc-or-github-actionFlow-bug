4e77e55f9ed1e13674fc91b40014d8798830a124
Yuria-Shikibe <3415339403@qq.com>
2024-03-13 23:05:40 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,6 @@ import ChronoTest.T01;
 import ChronoTest.T02;
 
 int main(){
-	Test::Test01 test01{};
-	std::cout << test01.getSpacing(std::chrono::milliseconds(10), std::chrono::microseconds(20)) << std::endl;
-
 	std::this_thread::sleep_for(std::chrono::microseconds(10));
 
 	Test::Test02 test02{};
