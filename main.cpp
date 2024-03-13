dadc13f69c75cd671f79d6e25f410c6169fd9412
Yuria-Shikibe <3415339403@qq.com>
2024-03-13 22:58:27 +0800

    -

--- main.cpp
+++ main.cpp
@@ -2,16 +2,25 @@
 #include <iostream>
 #include <thread>
 
-import ChronoTest.T01;
-import ChronoTest.T02;
+struct Test01 {
+	auto getSpacing(auto p1, auto p2){
+		return p1 - p2;
+	}
+};
+
+struct Test02 {
+	auto getSpacing(std::chrono::milliseconds p1, std::chrono::milliseconds p2){
+		return p1 - p2;
+	}
+};
 
 int main(){
-	Test::Test01 test01{};
+	Test01 test01{};
 	std::cout << test01.getSpacing(std::chrono::milliseconds(10), std::chrono::microseconds(20)) << std::endl;
 
 	std::cout << test01.getSpacing(std::chrono::microseconds(10), std::chrono::nanoseconds(20)) << std::endl;
 
-	Test::Test02 test02{};
+	Test02 test02{};
 	std::cout << test02.getSpacing(std::chrono::milliseconds(10), std::chrono::milliseconds(20)) << std::endl;
 	return 0;
 }
