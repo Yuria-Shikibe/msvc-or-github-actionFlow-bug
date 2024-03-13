e637aba73bcb6070a63862908d2847f12591d04c
Yuria-Shikibe <3415339403@qq.com>
2024-03-13 23:16:15 +0800

    -

--- main.cpp
+++ main.cpp
@@ -2,12 +2,19 @@
 #include <iostream>
 #include <thread>
 
+//Local Compile doesn't have any failure
+//But fails in Github Workflow
+//Use Both MSVC 17.9.5+33de0b227
+
+// Haven't Test Include Header File yet
+// Only Module triggers compile failure, currently
 import ChronoTest;
 
 int main(){
+	//must invoke this, should be the problem of -/+ operation between std::chrono::duration;
 	std::this_thread::sleep_for(std::chrono::microseconds(10));
 
-	Test::Test02 test02{};
+	constexpr Test::Test02 test02{};
 	std::cout << test02.getSpacing(std::chrono::milliseconds(10), std::chrono::milliseconds(20)) << std::endl;
 	return 0;
 }
--- src/ChronoTest.cppm
+++ src/ChronoTest.cppm
@@ -4,7 +4,7 @@ import <chrono>;
 
 export namespace Test{
 	struct Test02 {
-		auto getSpacing(const std::chrono::milliseconds p1, const std::chrono::milliseconds p2) const {
+		static auto getSpacing(const std::chrono::milliseconds p1, const std::chrono::milliseconds p2){
 			return p1 - p2;
 		}
 	};
